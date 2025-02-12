7bf7d3d9ed1981dd9310d6f5774dd2f3c9412381
abumatics <datradingbtkl@gmail.com>
2025-02-12 22:27:03 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { LogoIcon } from "./Icons";
 // import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
-// import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export const Footer = () => {
   return (
